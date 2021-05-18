82307c60c379a3923413edf8fb5b7b0c71bede63
xinyue-cwhy <2644965101@qq.com>
2021-05-18 14:46:45 +0800

    first commit

--- demo.php
+++ demo.php
@@ -1,6 +1,6 @@
 <?php
 header('content-type:application/json;charset=utf-8');
-$json=file_get_contents('https://xinyue-cwhy.github.io/robot/demo.json');
+$json=file_get_contents('./demo.json');
 sleep(1);
 echo $json;
 ?>
